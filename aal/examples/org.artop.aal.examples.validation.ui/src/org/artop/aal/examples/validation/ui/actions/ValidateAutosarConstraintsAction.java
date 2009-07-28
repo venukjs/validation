78403d67103e50fa8eeddc76dba1637fb92f042d
romain.sezestre <romain.sezestre@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2009-07-28 14:18:04 +0000

    Headers & comments minor modifications.

--- aal/examples/org.artop.aal.examples.validation.ui/src/org/artop/aal/examples/validation/ui/actions/ValidateAutosarConstraintsAction.java
+++ aal/examples/org.artop.aal.examples.validation.ui/src/org/artop/aal/examples/validation/ui/actions/ValidateAutosarConstraintsAction.java
@@ -1,14 +1,14 @@
 /**
  * <copyright>
  * 
- * Copyright (c) {contributing company name} and others.
+ * Copyright (c) BMW Car IT, Geensys and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Artop Software License 
  * Based on Released AUTOSAR Material (ASLR) which accompanies this 
  * distribution, and is available at http://www.artop.org/aslr.html
  * 
  * Contributors: 
- *     {contributing company name} - Initial API and implementation
+ *     Geensys - Initial API and implementation
  * 
  * </copyright>
  */
