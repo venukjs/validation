32645d23a6b5cf364d68a0aea8665a21d74d5d1d
yannick.didierjean <yannick.didierjean@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2009-11-30 08:39:57 +0000

    resolved  Bug 791   -  Move *.validation.tests.integration to validation sub-project

---
+++ ecl/tests/org.artop.ecl.emf.validation.tests.integration/src/org/artop/ecl/emf/validation/tests/integration/testsuite/EclEmfValidationTestsIntegrationTestSuite.java
@@ -0,0 +1,17 @@
+package org.artop.ecl.emf.validation.tests.integration.testsuite;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class EclEmfValidationTestsIntegrationTestSuite {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(
+				"Test for org.artop.ecl.emf.tests.testsuite");
+		// $JUnit-BEGIN$
+		// suite.addTestSuite(org.artop.aal.examples.converter.tests.integration.converters.Autosar212Converter21IntegrationTest.class);
+		// $JUnit-END$
+		return suite;
+	}
+
+}
