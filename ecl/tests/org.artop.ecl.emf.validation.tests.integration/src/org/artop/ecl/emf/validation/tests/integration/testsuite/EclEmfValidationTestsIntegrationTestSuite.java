696cd6b62ccd070b9fbb68990e61066d2690aa78
christian.knuechel <christian.knuechel@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2009-12-01 10:29:02 +0000

    Fixed copyright headers.

--- ecl/tests/org.artop.ecl.emf.validation.tests.integration/src/org/artop/ecl/emf/validation/tests/integration/testsuite/EclEmfValidationTestsIntegrationTestSuite.java
+++ ecl/tests/org.artop.ecl.emf.validation.tests.integration/src/org/artop/ecl/emf/validation/tests/integration/testsuite/EclEmfValidationTestsIntegrationTestSuite.java
@@ -1,3 +1,17 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) Geensys and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on Released
+ * AUTOSAR Material (ASLR) which accompanies this distribution, and is available
+ * at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     Geensys - Initial API and implementation
+ * 
+ * </copyright>
+ */
 package org.artop.ecl.emf.validation.tests.integration.testsuite;
 
 import junit.framework.Test;
