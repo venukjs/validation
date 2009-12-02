0cb3c75b61a3990416de652675a5196f07a22eda
stephan.eberle <stephan.eberle@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2009-12-02 14:21:52 +0000

    Initial import

---
+++ experimental/ecl/examples/org.artop.ecl.examples.sphinx.compare/src/org/artop/ecl/examples/sphinx/compare/match/SphinxMatchEngine.java
@@ -0,0 +1,24 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) Geensys and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on Released
+ * AUTOSAR Material (ASLR) which accompanies this distribution, and is available
+ * at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     Geensys - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.artop.ecl.examples.sphinx.compare.match;
+
+import org.artop.ecl.emf.compare.match.ModelMatchEngine;
+
+/**
+ * 
+ */
+public class SphinxMatchEngine extends ModelMatchEngine {
+
+}
