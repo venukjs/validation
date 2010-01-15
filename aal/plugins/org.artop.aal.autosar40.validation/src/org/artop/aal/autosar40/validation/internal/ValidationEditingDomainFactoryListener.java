08322c286916d75032ea5a13fc050da0185c6853
christian.knuechel <christian.knuechel@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2010-01-15 15:15:19 +0000

    Migration of preparatory autosar40 plug-ins into main repository. [bug 761]

---
+++ aal/plugins/org.artop.aal.autosar40.validation/src/org/artop/aal/autosar40/validation/internal/ValidationEditingDomainFactoryListener.java
@@ -0,0 +1,48 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) Geensys and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on Released
+ * AUTOSAR Material (ASLR) which accompanies this distribution, and is available
+ * at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     Geensys - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.artop.aal.autosar40.validation.internal;
+
+import org.artop.aal.autosar40.validation.listeners.AutomaticValidation40Listener;
+import org.artop.aal.autosar40.validation.listeners.ProblemMarkerIntegrity40Listener;
+import org.artop.ecl.emf.domain.factory.ITransactionalEditingDomainFactoryListener;
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+
+/**
+ * @since 1.2
+ */
+public class ValidationEditingDomainFactoryListener implements ITransactionalEditingDomainFactoryListener {
+
+	protected AutomaticValidation40Listener automaticValidationListener = new AutomaticValidation40Listener();
+	protected ProblemMarkerIntegrity40Listener problemMarkerListener = new ProblemMarkerIntegrity40Listener();
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void postCreateEditingDomain(TransactionalEditingDomain editingDomain) {
+		Assert.isNotNull(editingDomain);
+		editingDomain.addResourceSetListener(automaticValidationListener);
+		editingDomain.addResourceSetListener(problemMarkerListener);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void preDisposeEditingDomain(TransactionalEditingDomain editingDomain) {
+		Assert.isNotNull(editingDomain);
+		editingDomain.removeResourceSetListener(automaticValidationListener);
+		editingDomain.removeResourceSetListener(problemMarkerListener);
+	}
+}
