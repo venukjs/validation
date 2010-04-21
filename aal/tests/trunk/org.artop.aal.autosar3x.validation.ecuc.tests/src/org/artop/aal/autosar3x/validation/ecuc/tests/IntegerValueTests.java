840b9917b013d613db81ba12a257f3e84d30efef
broerkens <broerkens@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2010-04-21 16:52:23 +0000

    initial contribution of ECUC validation

---
+++ aal/tests/trunk/org.artop.aal.autosar3x.validation.ecuc.tests/src/org/artop/aal/autosar3x/validation/ecuc/tests/IntegerValueTests.java
@@ -0,0 +1,69 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) OpenSynergy and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     OpenSynergy - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.artop.aal.autosar3x.validation.ecuc.tests;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+
+@SuppressWarnings("nls")
+public class IntegerValueTests extends ValidationTestCase {
+
+	public IntegerValueTests() {
+		super();
+	}
+
+	@Override
+	protected String getConstraintID() {
+		return "org.artop.aal.autosar3x.validation.ecuc.IntegerValueConstraint_3x";//$NON-NLS-1$
+	}
+
+	// test completeness
+	public void testInvalidIntegerValue_noDefinition() throws Exception {
+		EObject invalidModel = loadInputFile("ecuc/IntegerValue/noDefinition.arxml");
+		ValidationTestUtil.validateModel(invalidModel, validator, IStatus.ERROR);
+	}
+
+	public void testInvalidIntegerValue_noValue() throws Exception {
+		EObject invalidModel = loadInputFile("ecuc/IntegerValue/noValue.arxml");
+		ValidationTestUtil.validateModel(invalidModel, validator, IStatus.ERROR);
+	}
+
+	public void testInvalidIntegerValue_invalidValue() throws Exception {
+		EObject invalidModel = loadInputFile("ecuc/IntegerValue/invalidValue.arxml");
+		ValidationTestUtil.validateModel(invalidModel, validator, IStatus.ERROR);
+	}
+
+	// test consistency
+	public void testInvalidIntegerValue_wrongParamDefType() throws Exception {
+		EObject invalidModel = loadInputFile("ecuc/IntegerValue/wrongParamDefType.arxml");
+		ValidationTestUtil.validateModel(invalidModel, validator, IStatus.ERROR);
+	}
+
+	// test correctness
+	public void testInvalidIntegerValue_valueLowerThanMin() throws Exception {
+		EObject invalidModel = loadInputFile("ecuc/IntegerValue/valueLowerThanMin.arxml");
+		ValidationTestUtil.validateModel(invalidModel, validator, IStatus.ERROR);
+	}
+
+	public void testInvalidIntegerValue_valueBiggerThanMax() throws Exception {
+		EObject invalidModel = loadInputFile("ecuc/IntegerValue/valueBiggerThanMax.arxml");
+		ValidationTestUtil.validateModel(invalidModel, validator, IStatus.ERROR);
+	}
+
+	public void testValidIntegerValue() throws Exception {
+		EObject validModel = loadInputFile("ecuc/IntegerValue/valid.arxml");
+		ValidationTestUtil.validateModel(validModel, validator, IStatus.OK);
+	}
+}
---
+++ aal/tests/trunk/org.artop.aal.autosar3x.validation.ecuc.tests/src/org/artop/aal/autosar3x/validation/ecuc/tests/testsuite/AalAutosar3xValidationEcucTestSuite.java
@@ -0,0 +1,48 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) OpenSynergy and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     OpenSynergy - Initial API and implementation
+ * 
+ * </copyright>
+ */
+
+package org.artop.aal.autosar3x.validation.ecuc.tests.testsuite;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.artop.aal.autosar3x.validation.ecuc.tests.BooleanValueTests;
+import org.artop.aal.autosar3x.validation.ecuc.tests.EnumerationValueTests;
+import org.artop.aal.autosar3x.validation.ecuc.tests.FloatValueTests;
+import org.artop.aal.autosar3x.validation.ecuc.tests.IntegerValueTests;
+import org.artop.aal.autosar3x.validation.ecuc.tests.ReferenceValueTests;
+import org.artop.aal.autosar3x.validation.ecuc.tests.StringValueTests;
+
+public class AalAutosar3xValidationEcucTestSuite {
+
+	/**
+	 * Suite.
+	 * 
+	 * @return the test
+	 */
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Tests for org.artop.aal.autosar3x.validation.ecuc"); //$NON-NLS-1$
+
+		// ecuc
+		suite.addTestSuite(IntegerValueTests.class);
+		suite.addTestSuite(StringValueTests.class);
+		suite.addTestSuite(BooleanValueTests.class);
+		suite.addTestSuite(FloatValueTests.class);
+		suite.addTestSuite(EnumerationValueTests.class);
+		suite.addTestSuite(ReferenceValueTests.class);
+
+		return suite;
+	}
+}
