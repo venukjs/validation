eba86b51d44a2837c68a62a084f122b2bbfa046d
broerkens <broerkens@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2010-04-23 21:02:38 +0000

    enabled container structural integrity constraint

--- aal/plugins/org.artop.aal.autosar3x.constraints.ecuc/src/org/artop/aal/autosar3x/constraints/ecuc/ContainerConstraint.java
+++ aal/plugins/org.artop.aal.autosar3x.constraints.ecuc/src/org/artop/aal/autosar3x/constraints/ecuc/ContainerConstraint.java
@@ -14,21 +14,13 @@
  */
 package org.artop.aal.autosar3x.constraints.ecuc;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.eclipse.emf.validation.IValidationContext;
 
 import autosar3x.ecucdescription.Container;
 import autosar3x.ecucdescription.EcucdescriptionPackage;
-import autosar3x.ecucdescription.ModuleConfiguration;
-import autosar3x.ecucparameterdef.ChoiceContainerDef;
 import autosar3x.ecucparameterdef.ContainerDef;
-import autosar3x.ecucparameterdef.ModuleDef;
-import autosar3x.ecucparameterdef.ParamConfContainerDef;
 
 public class ContainerConstraint extends AbstractModelConstraint {
 
@@ -38,9 +30,6 @@ public class ContainerConstraint extends AbstractModelConstraint {
 
 		Container container = (Container) ctx.getTarget();
 		IStatus status = validateDefinitionRef(ctx, container);
-		if (status.isOK()) {
-			status = validateContainmentStructure(ctx, container);
-		}
 
 		return status;
 	}
@@ -59,50 +48,6 @@ public class ContainerConstraint extends AbstractModelConstraint {
 			} else {
 				status = ctx.createSuccessStatus();
 			}
-		}
-		return status;
-	}
-
-	private IStatus validateContainmentStructure(IValidationContext ctx, Container container) {
-		final IStatus status;
-
-		EObject parent = container.eContainer();
-
-		if (null == parent) {
-			status = ctx.createFailureStatus("element has no parent");
-		} else {
-			final List<ContainerDef> containerDefList = new ArrayList<ContainerDef>();
-			ContainerDef containerDef = container.getDefinition();
-			if (parent instanceof ModuleConfiguration) {
-				// the current Container is directly contained in a ModuleConfiguration
-				ModuleConfiguration parentModule = (ModuleConfiguration) parent;
-				ModuleDef parentModuleDef = parentModule.getDefinition();
-				containerDefList.addAll(EcucUtil.getAllContainersOf(parentModuleDef));
-
-			} else if (parent instanceof Container) {
-				// the current Container is contained in another Container
-				Container parentContainer = (Container) parent;
-				ContainerDef parentContainerDef = parentContainer.getDefinition();
-
-				if (parentContainerDef instanceof ParamConfContainerDef) {
-					// the parent containers definition is a ParamConfContainerDef
-					ParamConfContainerDef parentParamConfContainerDef = (ParamConfContainerDef) parentContainerDef;
-					containerDefList.addAll(EcucUtil.getAllSubContainersOf(parentParamConfContainerDef));
-				} else if (parentContainerDef instanceof ChoiceContainerDef) {
-					// the parent containers definition is a ChoiceContainerDef
-					ChoiceContainerDef parentChoiceContainerDef = (ChoiceContainerDef) parentContainerDef;
-					containerDefList.addAll(EcucUtil.getAllChoicesOf(parentChoiceContainerDef));
-				}
-			}
-
-			if (0 == containerDefList.size()) {
-				status = ctx.createFailureStatus("containement problem: no container definitions found in parent definition"); //$NON-NLS-1$
-			} else if (!containerDefList.contains(containerDef)) {
-				status = ctx.createFailureStatus("containement problem: container definition of this container not found in parent definition"); //$NON-NLS-1$
-			} else {
-				status = ctx.createSuccessStatus();
-			}
-
 		}
 		return status;
 	}
