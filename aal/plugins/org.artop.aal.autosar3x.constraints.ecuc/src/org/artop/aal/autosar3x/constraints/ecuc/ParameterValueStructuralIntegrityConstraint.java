73b3eb45bcf8481da515eec78c63656522978b97
broerkens <broerkens@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2010-04-23 21:05:53 +0000

    copyright statements added

--- aal/plugins/org.artop.aal.autosar3x.constraints.ecuc/src/org/artop/aal/autosar3x/constraints/ecuc/ParameterValueStructuralIntegrityConstraint.java
+++ aal/plugins/org.artop.aal.autosar3x.constraints.ecuc/src/org/artop/aal/autosar3x/constraints/ecuc/ParameterValueStructuralIntegrityConstraint.java
@@ -1,3 +1,17 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) OpenSynergy and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     OpenSynergy - Initial API and implementation
+ * 
+ * </copyright>
+ */
 package org.artop.aal.autosar3x.constraints.ecuc;
 
 import org.eclipse.core.runtime.IStatus;
