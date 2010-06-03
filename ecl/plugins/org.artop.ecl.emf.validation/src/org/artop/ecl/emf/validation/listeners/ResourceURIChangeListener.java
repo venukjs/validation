fecd0bd6aa6591e69b47927599ab18f2ea77adc8
stephan.eberle <stephan.eberle@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2010-06-03 13:12:56 +0000

    RESOLVED - bug 1064: Incorporate findings from AUTOSAR IP clearance review https://www.artop.org/bugs/show_bug.cgi?id=1064

--- ecl/plugins/org.artop.ecl.emf.validation/src/org/artop/ecl/emf/validation/listeners/ResourceURIChangeListener.java
+++ ecl/plugins/org.artop.ecl.emf.validation/src/org/artop/ecl/emf/validation/listeners/ResourceURIChangeListener.java
@@ -82,7 +82,6 @@ public class ResourceURIChangeListener implements IResourceChangeListener {
 	private void updateCache(IResourceChangeEvent event) {
 		IResourceDelta rootDelta = event.getDelta();
 
-		// final ArrayList<IResource> changed = new ArrayList<IResource>();
 		IResourceDeltaVisitor visitor = new IResourceDeltaVisitor() {
 			public boolean visit(IResourceDelta delta) {
 				IResource resource = delta.getResource();
@@ -94,8 +93,8 @@ public class ResourceURIChangeListener implements IResourceChangeListener {
 				if ((delta.getFlags() & IResourceDelta.MARKERS) == 0) {
 					return true;
 				}
-				// only interested in files with the "arxml" extension
-				if (resource.getType() == IResource.FILE && "arxml".equalsIgnoreCase(resource.getFileExtension()) && resource.exists()) { //$NON-NLS-1$
+				// only interested in files that exist
+				if (resource.getType() == IResource.FILE && resource.exists()) {
 					cache.add(resource);
 				}
 				return true;
