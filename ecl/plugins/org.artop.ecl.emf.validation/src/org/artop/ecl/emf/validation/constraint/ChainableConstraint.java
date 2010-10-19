5f62d5f1150a588e85a124db2ef32c465fdcda3c
paul.hoser <paul.hoser@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2010-10-19 13:04:39 +0000

    Added javadocs

--- ecl/plugins/org.artop.ecl.emf.validation/src/org/artop/ecl/emf/validation/constraint/ChainableConstraint.java
+++ ecl/plugins/org.artop.ecl.emf.validation/src/org/artop/ecl/emf/validation/constraint/ChainableConstraint.java
@@ -22,12 +22,20 @@ public abstract class ChainableConstraint extends AbstractModelConstraint implem
 	 * Returns the actual Constraint instance. Any call to the ChainableConstraint will be delegated to the Constraint
 	 * returned by this method unless one of the provided ConstraintCallHandlers already handles the call.
 	 * 
-	 * @return
+	 * @return The Constraint which is to be called by default if none of the appended ConstraintCallHandlers can handle
+	 *         the validate()-call.
 	 */
 	protected AbstractModelConstraint getDelegateConstraint() {
 		return new SuccessConstraint();
 	}
 
+	/**
+	 * Returns the type of ConstraintCallHandlers which this Constraint will forward validate()-calls to.
+	 * ConstraintCallHandlers which want to be appended to this Constraint have to be instanceof the returned class.
+	 * Constraints which are intended to be extended by ConstraintCallHandlers have to override this method.
+	 * 
+	 * @return The class of which ConstraintCallHandlers have be instanceof in order to be appended to this Constraint.
+	 */
 	protected Class<? extends IConstraintCallHandler> getSupportedHandlerClass() {
 		return null;
 	}
