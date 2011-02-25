6526b0029a680755e9ef512faeafb69008124254
christian.knuechel <christian.knuechel@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-02-25 11:15:08 +0000

    Fixed copyright headers.

--- aal/tests/org.artop.aal.validation.constraints.swc.tests/src/org/artop/aal/validation/constraints/swc/mock/MockSwcPredicatesServiceProvider.java
+++ aal/tests/org.artop.aal.validation.constraints.swc.tests/src/org/artop/aal/validation/constraints/swc/mock/MockSwcPredicatesServiceProvider.java
@@ -1,3 +1,17 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) BMW Car IT and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     BMW Car IT - Initial API and implementation
+ * 
+ * </copyright>
+ */
 package org.artop.aal.validation.constraints.swc.mock;
 
 import gautosar.gswcomponents.gportinterface.GClientServerInterface;
