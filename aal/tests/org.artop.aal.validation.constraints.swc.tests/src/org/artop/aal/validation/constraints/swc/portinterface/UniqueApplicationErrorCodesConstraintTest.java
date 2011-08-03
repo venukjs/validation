fa62b039ea11c5902aab52b32ce2601c542a9884
paul.hoser <paul.hoser@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-08-03 08:52:33 +0000

    Adjusted tests after Predicate is not retrieved from Service anymore

--- aal/tests/org.artop.aal.validation.constraints.swc.tests/src/org/artop/aal/validation/constraints/swc/portinterface/UniqueApplicationErrorCodesConstraintTest.java
+++ aal/tests/org.artop.aal.validation.constraints.swc.tests/src/org/artop/aal/validation/constraints/swc/portinterface/UniqueApplicationErrorCodesConstraintTest.java
@@ -14,55 +14,61 @@
  */
 package org.artop.aal.validation.constraints.swc.portinterface;
 
-import org.junit.Assert;
+import static org.artop.aal.gautosar.services.builder.swc.portinterface.GClientServerInterfaceBuilder.clientServerInterface;
+import gautosar.gswcomponents.gportinterface.GClientServerInterface;
+
+import org.artop.aal.gautosar.services.builder.GMaker;
+import org.artop.aal.gautosar.services.builder.swc.portinterface.GClientServerInterfaceBuilder;
+import org.artop.aal.validation.testutils.PredicateBasedConstraintTest;
+import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.junit.Test;
 
+import autosar40.util.Autosar40ReleaseDescriptor;
+
 /**
- * Tests if the failure messages returned by the <code>UniqueApplicationErrorCodesConstraint</code> are correct.
+ * Tests if the failure messages returned by the <code>UniqueApplicationErrorCodesConstraint</code> are correct. This
+ * test shall not test the logic of the Constraint as the logic is implemented in the HasUniqueErrorCodesPredicate. The
+ * UniqueApplicationErrorCodesConstraint only wraps the Predicate and provides the messages.
  */
 @SuppressWarnings("nls")
-public class UniqueApplicationErrorCodesConstraintTest /* extends PredicateBasedConstraintTest */{
+public class UniqueApplicationErrorCodesConstraintTest extends PredicateBasedConstraintTest {
 
-	// @Override
-	// protected AbstractModelConstraint createConstraintUnderTest() {
-	// return new UniqueApplicationErrorCodesConstraint();
-	// }
-
-	@Test
-	public void disabledTest() {
-		Assert.fail("Please visit this class and fix the compilation errors");
+	@Override
+	protected AbstractModelConstraint createConstraintUnderTest() {
+		return new UniqueApplicationErrorCodesConstraint();
 	}
 
-	// @Test
-	// public void shouldPassWithUniqueErrorCodes() {
-	// getSwcServiceProvider().setHasUniqueErrorCodesPredicate(uniqueErrorCodes());
-	// assertSuccessfulValidation();
-	// }
-	//
-	// @Test
-	// public void shouldCreateOneFailureMessageWithTwoErrorCodes() {
-	// getSwcServiceProvider().setHasUniqueErrorCodesPredicate(nonUniqueErrorCodes(multiplyAssignedErrorCodeValue(0,
-	// "IDLE", "INIT")));
-	// String expectedMessage = "More than one ApplicationErrorCode has the value \"0\": IDLE, INIT";
-	// assertFailedValidation(expectedMessage);
-	// }
-	//
-	// @Test
-	// public void shouldCreateOneFailureMessageWithThreeErrorCodes() {
-	// getSwcServiceProvider().setHasUniqueErrorCodesPredicate(nonUniqueErrorCodes(multiplyAssignedErrorCodeValue(0,
-	// "IDLE", "INIT", "RUNNING")));
-	// String expectedMessage = "More than one ApplicationErrorCode has the value \"0\": IDLE, INIT, RUNNING";
-	// assertFailedValidation(expectedMessage);
-	// }
-	//
-	// @Test
-	// public void shouldCreateTwoFailureMessages() {
-	// getSwcServiceProvider().setHasUniqueErrorCodesPredicate(
-	// nonUniqueErrorCodes(multiplyAssignedErrorCodeValue(0, "IDLE", "INIT"), multiplyAssignedErrorCodeValue(1,
-	// "RUNNING", "WAITING")));
-	// String expectedMessage1 = "More than one ApplicationErrorCode has the value \"0\": IDLE, INIT";
-	// String expectedMessage2 = "More than one ApplicationErrorCode has the value \"1\": RUNNING, WAITING";
-	// assertFailedValidation(expectedMessage1, expectedMessage2);
-	// }
+	@Test
+	public void shouldPassWithUniqueErrorCodes() {
+		assertSuccessfulValidation(make(clientServerInterface("CSIfc")));
+	}
+
+	@Test
+	public void shouldCreateOneFailureMessageWithTwoErrorCodes() {
+		GClientServerInterfaceBuilder csIfc = clientServerInterface("csIfc").possibleError("WRONG_STATE", 0).possibleError("NOT_INITIALIZED", 0);
+		String expectedMessage = "More than one ApplicationErrorCode has the value \"0\": WRONG_STATE, NOT_INITIALIZED";
+		assertFailedValidation(make(csIfc), expectedMessage);
+	}
+
+	@Test
+	public void shouldCreateOneFailureMessageWithThreeErrorCodes() {
+		GClientServerInterfaceBuilder csIfc = clientServerInterface("csIfc").possibleError("WRONG_STATE", 0).possibleError("NOT_INITIALIZED", 0)
+				.possibleError("NEGATIVE_VALUE", 0);
+		String expectedMessage = "More than one ApplicationErrorCode has the value \"0\": WRONG_STATE, NOT_INITIALIZED, NEGATIVE_VALUE";
+		assertFailedValidation(make(csIfc), expectedMessage);
+	}
+
+	@Test
+	public void shouldCreateTwoFailureMessages() {
+		GClientServerInterfaceBuilder csIfc = clientServerInterface("csIfc").possibleError("WRONG_STATE", 0).possibleError("NOT_INITIALIZED", 0)
+				.possibleError("NEGATIVE_VALUE", 1).possibleError("DIVISION_BY_ZERO", 1);
+		String expectedMessage1 = "More than one ApplicationErrorCode has the value \"0\": WRONG_STATE, NOT_INITIALIZED";
+		String expectedMessage2 = "More than one ApplicationErrorCode has the value \"1\": NEGATIVE_VALUE, DIVISION_BY_ZERO";
+		assertFailedValidation(make(csIfc), expectedMessage1, expectedMessage2);
+	}
+
+	private GClientServerInterface make(GClientServerInterfaceBuilder builder) {
+		return GMaker.make(Autosar40ReleaseDescriptor.INSTANCE).from(builder);
+	}
 
 }
