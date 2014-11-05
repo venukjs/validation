f7ac653caf65569119399286ba471a23539490b5
Florina Craciunescu <florina.craciunescu@conti-engineering.com>
2014-11-05 11:46:16 +0200

    Bug 2038 - updated validation rules and added tests

--- aal/plugins/org.artop.aal.autosar40.constraints.ecuc/src/org/artop/aal/autosar40/constraints/ecuc/EcucParameterDefImplConfigClassConstraint.java
+++ aal/plugins/org.artop.aal.autosar40.constraints.ecuc/src/org/artop/aal/autosar40/constraints/ecuc/EcucParameterDefImplConfigClassConstraint.java
@@ -19,6 +19,8 @@ import gautosar.gecucparameterdef.GConfigParameter;
 import org.artop.aal.gautosar.constraints.ecuc.AbstractGConfigParameterImplConfigClassConstraint;
 import org.eclipse.emf.common.util.EList;
 
+import autosar40.ecucparameterdef.EcucConfigurationClassEnum;
+import autosar40.ecucparameterdef.EcucConfigurationVariantEnum;
 import autosar40.ecucparameterdef.EcucImplementationConfigurationClass;
 import autosar40.ecucparameterdef.EcucParameterDef;
 
@@ -51,22 +53,41 @@ public class EcucParameterDefImplConfigClassConstraint extends AbstractGConfigPa
 		} else {
 			valid = configClassList1.size() == configClassList2.size();
 			if (valid) {
-				for (int i = 0; i < configClassList1.size(); i++) {
-					EcucImplementationConfigurationClass cfClass1 = configClassList1.get(i);
-					EcucImplementationConfigurationClass cfClass2 = configClassList2.get(i);
-					if (cfClass1 == null) {
-						valid = cfClass2 == null;
-					} else {
-						valid = cfClass1.equals(cfClass2);
-					}
-					if (valid == false) {
-						break;
-					}
-				}
+				valid = compareEcucImplementationConfigClass(configClassList1, configClassList2);
 			}
 		}
 
 		return valid;
+	}
+
+	private boolean compareEcucImplementationConfigClass(EList<EcucImplementationConfigurationClass> configClassList1,EList<EcucImplementationConfigurationClass> configClassList2) {
+		boolean elementIsValid = false;
+		for (int i = 0; i < configClassList1.size(); i++) {
+			EcucImplementationConfigurationClass cfClass1 = configClassList1.get(i);
+			EcucImplementationConfigurationClass cfClass2 = configClassList2.get(i);
+			if (cfClass1 == null) {
+				elementIsValid = cfClass2 == null;
+			} else {
+				EcucConfigurationClassEnum configClass1 = cfClass1.getConfigClass();
+				EcucConfigurationClassEnum configClass2 = cfClass2.getConfigClass();
+				if((configClass1!=null)&&(configClass2!=null)){
+					elementIsValid=configClass1.equals(configClass2);
+					if(elementIsValid){
+						EcucConfigurationVariantEnum configVariant1 = cfClass1.getConfigVariant();
+						EcucConfigurationVariantEnum configVariant2 = cfClass1.getConfigVariant();
+						if((configVariant1!=null)&&(configVariant2!=null)){
+							elementIsValid=configVariant1.equals(configVariant2);
+						}
+					}
+				}
+				
+
+			}
+			if (elementIsValid == false) {
+				break;
+			}
+		}
+		return elementIsValid;
 	}
 
 }
