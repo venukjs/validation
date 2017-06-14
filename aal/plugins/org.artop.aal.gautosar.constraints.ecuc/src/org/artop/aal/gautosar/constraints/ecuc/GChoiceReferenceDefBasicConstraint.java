1c35e6a157b38e17b459b627da258d579cab49a5
Christian Becker <christian.becker@bosch.com>
2017-06-14 13:57:02 +0200

    fix Bug 2192 ClassCastException occurs during ARTOP type system creation

--- aal/plugins/org.artop.aal.gautosar.constraints.ecuc/src/org/artop/aal/gautosar/constraints/ecuc/GChoiceReferenceDefBasicConstraint.java
+++ aal/plugins/org.artop.aal.gautosar.constraints.ecuc/src/org/artop/aal/gautosar/constraints/ecuc/GChoiceReferenceDefBasicConstraint.java
@@ -1,23 +1,19 @@
 /**
  * <copyright>
- * 
+ *
  * Copyright (c) OpenSynergy, Continental Engineering Services and others.
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Artop Software License Based on AUTOSAR
  * Released Material (ASLR) which accompanies this distribution, and is
  * available at http://www.artop.org/aslr.html
- * 
- * Contributors: 
+ *
+ * Contributors:
  *     OpenSynergy - Initial API and implementation for AUTOSAR 3.x
  *     Continental Engineering Services - migration to gautosar
- * 
+ *
  * </copyright>
  */
 package org.artop.aal.gautosar.constraints.ecuc;
-
-import gautosar.gecucdescription.GParameterValue;
-import gautosar.gecucparameterdef.GChoiceReferenceDef;
-import gautosar.gecucparameterdef.GParamConfContainerDef;
 
 import org.artop.aal.common.resource.AutosarURIFactory;
 import org.artop.aal.gautosar.constraints.ecuc.internal.Activator;
@@ -26,6 +22,10 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.emf.validation.IValidationContext;
 import org.eclipse.osgi.util.NLS;
+
+import gautosar.gecucdescription.GParameterValue;
+import gautosar.gecucparameterdef.GChoiceReferenceDef;
+import gautosar.gecucparameterdef.GContainerDef;
 
 /**
  * Superclass for the constraints implementations on reference def.
@@ -45,7 +45,7 @@ public class GChoiceReferenceDefBasicConstraint extends AbstractGParameterValueC
 		if (gChoiceReferenceDef.gGetDestinations().isEmpty()) {
 			multiStatus.add(ctx.createFailureStatus(EcucConstraintMessages.choiceref_emptyDestination));
 		} else {
-			for (GParamConfContainerDef choiceParamConfContainerDef : gChoiceReferenceDef.gGetDestinations()) {
+			for (GContainerDef choiceParamConfContainerDef : gChoiceReferenceDef.gGetDestinations()) {
 				if (choiceParamConfContainerDef.eIsProxy()) {
 					multiStatus.add(ctx.createFailureStatus(NLS.bind(EcucConstraintMessages.reference_destinationNotResolved,
 							AutosarURIFactory.getAbsoluteQualifiedName(choiceParamConfContainerDef))));
